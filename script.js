14214fe3d224a4f2b92aaee2f49eff1ecbccc55d
maina2 <chanzusimon6@gmail.com>
2023-03-21 23:30:53 +0300

    boot camp testimonial slider hub upload

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const slider = document.querySelector(".slider");
+const slider = document.querySelector('.slider');
 
-const leftarrow = document.querySelector(".left");
-const rightarrw = document.querySelector(".right");
+const leftarrow = document.querySelector( '.arrow left');
+const rightarrow = document.querySelector('.arrow right');
 
-var sectionindex = 0;
+var sectionIndex = 0;
 
-leftarrow.addEventListener('clcik',function() {
+leftarrow.addEventListener('clcik',function(){
 
-    sectionIndex = (sectionindex > 0 ) ? sectionIndex-1 : 0,
-    slider,style,transfrom = 'translate('+(sectionIndex) + -50 + ' %)' ,
+    sectionIndex = (sectionIndex > 0 ) ? sectionIndex-1 : 0;
+    slider.style.transfrom = 'translate(' + (sectionIndex) * 50 + '%)';
     
 });
 
 rightarrow.addEventListener('clcik',function() {
 
-    sectionIndex = (sectionIndex < 2 ) ? sectionIndex+1 : 0,
-    slider,style,transfrom = 'translate('+(sectionIndex) + -50 + ' %)' ,
+    sectionIndex = (sectionIndex < 1 ) ? sectionIndex + 1 : 0;
+    slider.style.transfrom = 'translate(' + (sectionIndex) * 50 + '%)';
     
 });
